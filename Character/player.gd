aa73e31450d607e34c822e5fa6a938be909cbadf
ki11errabbit <ki11errabbit@posteo.net>
2024-12-15 10:23:38 -0700

    redid directional movement code

--- Character/player.gd
+++ Character/player.gd
@@ -3,11 +3,15 @@ extends CharacterBody2D
 var BaseCharacter = preload("res://Character/BaseCharacter.tscn").instantiate()
 
 const SPEED = 300.0
+const DIRECTION_RESTRICT = 0.8
+const LOOK_RESTRICT = 0.8
+
 # Has type BaseCharacter.WalkingDirection
 var walking_direction = BaseCharacter.WalkingDirection.Forwards
 
 @onready
 var player_model = $BaseCharacter
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -23,54 +27,115 @@ func _process(delta: float) -> void:
 	var normalized_aim = aim_direction.normalized()
 	#print(move_direction)
 	#print(normalized_move)
-	if normalized_move.y > 0 and (normalized_move.x < 0.4 and normalized_move.x > -0.4):
-		if normalized_aim.dot(normalized_move) > 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Forwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Down)
-			print("forwards")
-		elif normalized_aim.dot(normalized_move) < 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Backwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Up)
-			print("backwards")
-	elif normalized_move.x > 0 and (normalized_move.y < 0.4 and normalized_move.y > -0.4):
-		if normalized_aim.dot(normalized_move) > 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Forwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Right)
-			print("forwards")
-		elif normalized_aim.dot(normalized_move) < 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Backwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Left)
-			print("backwards")
-	elif normalized_move.x < 0 and (normalized_move.y < 0.4 and normalized_move.y > -0.4):
-		if normalized_aim.dot(normalized_move) > 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Forwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Left)
-			print("forwards")
-		elif normalized_aim.dot(normalized_move) < 0.9:
-			walking_direction = BaseCharacter.WalkingDirection.Backwards
-			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Right)
-			print("backwards")
-	elif normalized_move.y > 0 and normalized_move.x >= 0.4:
-		print("diagonal")
-		pass
-	elif normalized_move.y > 0 and normalized_move.x <= -0.4:
-		print("diagonal")
-		pass
-	elif normalized_move.y < 0 and (normalized_move.x < 0.4 and normalized_move.x > -0.4):
-		if normalized_aim.dot(normalized_move) > 0.9:
+	
+	#print(aim_direction)
+	#print(rad_to_deg(Vector2.RIGHT.rotated(deg_to_rad(-45)).normalized().angle()))
+	#print(rad_to_deg(Vector2.RIGHT.rotated(deg_to_rad(45)).normalized().angle()))
+
+	# Moving Downwards
+	if Vector2.DOWN.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Down")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
 			walking_direction = BaseCharacter.WalkingDirection.Forwards
 			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Up)
 			print("forwards")
-		elif normalized_aim.dot(normalized_move) < 0.9:
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
 			walking_direction = BaseCharacter.WalkingDirection.Backwards
 			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Down)
 			print("backwards")
-	elif normalized_move.y < 0 and normalized_move.x >= 0.4:
-		print("diagonal")
-		pass
-	elif normalized_move.y < 0 and normalized_move.x <= -0.4:
-		print("diagonal")
-		pass
+	# Moving Upwards
+	elif Vector2.UP.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Up")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Down)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Up)
+			print("backwards")
+	# Moving to the Right
+	elif Vector2.RIGHT.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Right)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Left)
+			print("backwards")
+	# Moving to the Left
+	elif Vector2.LEFT.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Left)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.Right)
+			print("backwards")
+	# Moving to the Top Right
+	elif Vector2.RIGHT.rotated(deg_to_rad(-45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Top Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.UpRight)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.DownLeft)
+			print("backwards")
+	# Moving to the Bottom Right
+	elif Vector2.RIGHT.rotated(deg_to_rad(45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Bottom Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.DownRight)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.UpLeft)
+			print("backwards")
+	# Moving to the Top Left
+	elif Vector2.LEFT.rotated(deg_to_rad(45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Top Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.UpLeft)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.DownRight)
+			print("backwards")
+	# Moving to the Bottom Left
+	elif Vector2.LEFT.rotated(deg_to_rad(-45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Bottom Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Forwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.DownLeft)
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			walking_direction = BaseCharacter.WalkingDirection.Backwards
+			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.UpRight)
+			print("backwards")
 	else:
 		player_model.stop_moving()
 		var angle = aim_direction.angle()
@@ -102,14 +167,7 @@ func _process(delta: float) -> void:
 		elif angle > 100 and angle < 160:
 			print("diagonal down left")
 			player_model.set_current_facing_direction(BaseCharacter.FacingDirection.DownLeft)
-	"""
-	if normalized_aim.dot(normalized_move) > 0.9:
-		walking_direction = BaseCharacter.WalkingDirection.Forwards
-		print("forwards")
-	elif normalized_aim.dot(normalized_move) < 0.9:
-		walking_direction = BaseCharacter.WalkingDirection.Backwards
-		print("backwards")
-	"""
+	
 	
 	var movement_vec = move_direction * 100
 	#print(movement_vec)
