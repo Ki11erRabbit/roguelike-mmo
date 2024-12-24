98446383b704d44f56916c3a538bf0112a5f5a38
ki11errabbit <unlikelytitan@gmail.com>
2024-12-24 10:56:40 -0800

    added right stick rotation

--- Character/player.gd
+++ Character/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
+@onready
+var model = $BaseCharacter
 
 const SPEED = 5.0
 #const SPEED = 300.0
 const DIRECTION_RESTRICT = 0.8
 const LOOK_RESTRICT = 0.8
 const JUMP_VELOCITY = 4.5
+
+var last_aim: Vector2 = Vector2(0, 0)
 
 
 func _physics_process(delta: float) -> void:
@@ -14,18 +18,136 @@ func _physics_process(delta: float) -> void:
 	var aim_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
 	var normalized_aim = aim_direction.normalized()
 	
+	var rotating: bool = false
+	var clockwise_rotation = is_rotating_clockwise(aim_direction.angle(), last_aim.angle())
+	
+	if aim_direction.x == 0 and aim_direction.y == 0:
+		aim_direction = last_aim
+		normalized_aim = last_aim.normalized()
+	
+	#current_aim_angle = lerp_angle(last_aim_angle, current_aim_angle, 0.1)
+	model.rotation.y = atan2(aim_direction.x, aim_direction.y)
+	
+	# TODO: come up with better condition for checking for roatation
+	if true:
+		last_aim = aim_direction
+		pass
+	else:
+		rotating = true
+	
+	var moving: bool = false
+	
 	var movement_vec = move_direction * 100
 	#print(movement_vec)
 	
 	if (movement_vec.y > 50 or movement_vec.y < -50) or (movement_vec.x > 50 or movement_vec.x < -50):
-		$BaseCharacter.start_running()
+		model.start_running()
+		moving = true
 		print("running")
 	elif movement_vec.x != 0 or movement_vec.y != 0:
-		$BaseCharacter.start_walking()
+		model.start_walking()
+		moving = true
 		print("walking")
 	else:
-		$BaseCharacter.start_standing()
+		model.start_standing()
+		moving = false
 		print("standing")
+	
+	# Moving Downwards
+	if Vector2.DOWN.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Down")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving Upwards
+	elif Vector2.UP.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Up")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Right
+	elif Vector2.RIGHT.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Left
+	elif Vector2.LEFT.normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Top Right
+	elif Vector2.RIGHT.rotated(deg_to_rad(-45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Top Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Bottom Right
+	elif Vector2.RIGHT.rotated(deg_to_rad(45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Bottom Right")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Top Left
+	elif Vector2.LEFT.rotated(deg_to_rad(45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Top Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+			model.start_backstepping()
+	# Moving to the Bottom Left
+	elif Vector2.LEFT.rotated(deg_to_rad(-45)).normalized().dot(normalized_move) >= DIRECTION_RESTRICT:
+		print("Bottom Left")
+		if normalized_aim.x == 0 and normalized_aim.y == 0:
+			pass
+		elif normalized_aim.dot(normalized_move) >= LOOK_RESTRICT:
+			print("forwards")
+		elif normalized_aim.dot(normalized_move) <= LOOK_RESTRICT:
+			print("backwards")
+	else:
+		model.start_standing()
+		if rotating and not moving:
+			if clockwise_rotation:
+				model.rotate_right()
+			else:
+				model.rotate_left()
+		else:
+			model.stop_rotating()
+			model.start_standing()
+		
+	
+
+	
+	
+	
 	
 	return
 	
@@ -49,3 +171,22 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func is_rotating_clockwise(current, last) -> bool:
+	current = rad_to_deg(current)
+	last = rad_to_deg(last)
+	
+	if last > 0:
+		if current > last:
+			return false
+		else:
+			return true
+	elif last < 0:
+		if current > last:
+			return false
+		else:
+			return true
+	else:
+		return false
+	
+	
